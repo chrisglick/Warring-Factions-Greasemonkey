b9cafe7d3f4cbd9733dc15e10641d3c382c2c1e8
chrisglick <chrisglick@gmail.com>
2015-12-22 22:36:51 -0700

    Removed perim scan title and added more position replacements
    Cut the first table which is the sort by (use the icons)
    Cut the perimeter scan title on top
    Shortened empire names to initials
    Added Open Space, System, Alpha, Proto-arm, and Arm replacements

--- Warfacts_smaller_perimeter_scan.user.js
+++ Warfacts_smaller_perimeter_scan.user.js
@@ -20,27 +20,33 @@
 //alert('Monkey sez... "Hello World!"');
 
 //hide the sort table
-document.getElementsByTagName("table")[0].innerHTML = "";
+var firstTable = document.getElementsByTagName("table")[0];
+firstTable.parentNode.removeChild(firstTable);
 
-//Each starlog entry is a div, grab them all
-var tds = document.getElementById("perimTable").getElementsByTagName("tbody")[0].getElementsByTagName("td");
+var h1 = document.getElementsByTagName("h1")[0];
+h1.parentNode.removeChild(h1);
+
+
 var i;
-var currentValue;
 
 
-//For each starlog entry
- for (i = 0; i < tds.length; i+=1) {	//i=3 because first are headers 
+var tds = document.getElementById("perimTable").getElementsByTagName("tbody")[0].getElementsByTagName("td");
+ for (i = 0; i < tds.length; i+=1) {
 
 
 	// 0 = fleet name
 	// 1 = user
 	if (i % 6 == 1) {
 		splitLines = tds[i].innerHTML.split('<br>');
-		tds[i].innerHTML = splitLines[2].replace('<font color="#CC0000">Scaldarians</font>', '<font color="#CC0000">&bull;</font> ') + splitLines[0] + " [" + splitLines[1] + "]";
+		splitEmpire = splitLines[1].split(' ');
+		shortEmpire = "";
+		for (word of splitEmpire) {
+			shortEmpire += word.substring(0,1);
+		}
+		
+		tds[i].innerHTML = splitLines[2].replace('<font color="#CC0000">Scaldarians</font>', '<font color="#CC0000">&bull;</font> ') + splitLines[0] + " <span title='" + splitLines[1] + "'>[" + shortEmpire + "]</span>";
 	}
 	
-
-
 	// 2 = # ships
 	// 3 = tonnage
 	// 4 = distance
@@ -51,9 +57,13 @@ var currentValue;
 	// 5 = position
 	if (i % 6 == 5) {
 		pos = tds[i].innerHTML;
-		pos = pos.replace("Colony:<br>", "&Delta; ");
+		pos = pos.replace("Colony:<br>", "&prod; ");
 		pos = pos.replace("Orbit:<br>", "&#x2295; ");
-		pos = pos.replace("Open Space:<br>", "&there4; ");
+		pos = pos.replace("Open Space:<br>", "&Xi; ");
+		pos = pos.replace("System:<br>", "&there4; ");
+		pos = pos.replace("Alpha", "&Acirc;");
+		pos = pos.replace("Proto-arm", "Pa");
+		pos = pos.replace("-A", "-");
 		
 		tds[i].innerHTML = pos;
 	}
